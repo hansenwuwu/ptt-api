90127e8a41d7c00c03921d71834a1c824bfd2d5b
hansenwuwu <eric248655665@gmail.com>
2021-01-07 23:21:12 +0800

    Fix testing

--- main.py
+++ main.py
@@ -1,24 +1,33 @@
-from fastapi import fastapi
+# from fastapi import fastapi
 
-import json
-import requests
-from bs4 import BeautifulSoup
+# import json
+# import requests
+# from bs4 import BeautifulSoup
+
+# app = FastAPI()
+
+# PTT_URL = 'https://www.ptt.cc'
+
+# @app.get('/')
+# @app.get("/api/v1")
+# async def root():
+#     return {"message": "Hello World"}
+
+# @app.get("/api/v1/popular-forum")
+# async def popular_forum():
+#     content = requests.get(
+#             url= PTT_URL + '/bbs/index.html',
+#             cookies={'over18': '1'}, timeout=3
+#         ).content.decode('utf-8')
+#     soup = BeautifulSoup(content, 'html.parser')
+    
+#     return {"message": "Hello World"}
+
+from fastapi import FastAPI
 
 app = FastAPI()
 
-PTT_URL = 'https://www.ptt.cc'
 
-@app.get('/')
-@app.get("/api/v1")
-async def root():
-    return {"message": "Hello World"}
-
-@app.get("/api/v1/popular-forum")
-async def popular_forum():
-    content = requests.get(
-            url= PTT_URL + '/bbs/index.html',
-            cookies={'over18': '1'}, timeout=3
-        ).content.decode('utf-8')
-    soup = BeautifulSoup(content, 'html.parser')
-    
-    return {"message": "Hello World"}
+@app.get("/")
+async def read_main():
+    return {"msg": "Hello World"}
